ae5ba315b6a0342b8d29827db5edbb45e8a2fc46
Daniel Morandini <danielmorandini@me.com>
2019-01-30 22:59:39 +0100

    Log request method

--- nexmo/router.go
+++ nexmo/router.go
@@ -142,7 +142,7 @@ func makePlayRecordingHandler(hostAddr string) http.HandlerFunc {
 func loggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Do stuff here
-		log.Println(r.RequestURI)
+		log.Printf("[%s] %s", r.Method, r.RequestURI)
 		// Call the next handler, which can be another middleware in the chain, or the final handler.
 		next.ServeHTTP(w, r)
 	})
