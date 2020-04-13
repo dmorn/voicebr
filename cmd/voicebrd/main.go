0cfcbc35782e2e136dd6900f28d5b695de76d65f
Daniel Morandini <danielmorandini@me.com>
2020-04-13 09:40:33 +0200

    Add ReturnHandlerFunc

--- cmd/voicebrd/main.go
+++ cmd/voicebrd/main.go
@@ -31,7 +31,16 @@ func Error(w http.ResponseWriter, err error) {
 	http.Error(w, err.Error(), code)
 }
 
-func returnAnswer(w http.ResponseWriter, r *http.Request) error {
+type ReturnHandlerFunc func(w http.ResponseWriter, r *http.Request) error
+
+func (h ReturnHandlerFunc) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if err := h(w, r); err != nil {
+		Error(w, err)
+		return
+	}
+}
+
+func Answer(w http.ResponseWriter, r *http.Request) error {
 	defer r.Body.Close()
 	b, err := ioutil.ReadAll(r.Body)
 	if err != nil {
@@ -47,22 +56,14 @@ func returnAnswer(w http.ResponseWriter, r *http.Request) error {
 
 	log.Printf("*** reveived call (%v) from: %v", p.UUID, p.From)
 
-	ncco := vonage.NewTalkControl("Hi from Jecoz")
-	if err = json.NewEncoder(w).Encode(&ncco); err != nil {
+	nccos := []interface{}{vonage.NewTalkControl("Hi from Jecoz")}
+	if err = json.NewEncoder(w).Encode(nccos); err != nil {
 		return fmt.Errorf("answer response: %w", err)
 	}
 	return nil
 }
 
-func Answer(w http.ResponseWriter, r *http.Request) {
-	if err := returnAnswer(w, r); err != nil {
-		Error(w, err)
-	}
-	// Just need to write a response, http.StatusOK will be
-	// send automatically.
-}
-
-func returnEvent(w http.ResponseWriter, r *http.Request) error {
+func Event(w http.ResponseWriter, r *http.Request) error {
 	defer r.Body.Close()
 	b, err := ioutil.ReadAll(r.Body)
 	if err != nil {
@@ -77,22 +78,13 @@ func returnEvent(w http.ResponseWriter, r *http.Request) error {
 	}
 
 	log.Printf("event: %v", p)
-	w.WriteHeader(http.StatusOK)
 	return nil
-}
-
-func Event(w http.ResponseWriter, r *http.Request) {
-	if err := returnEvent(w, r); err != nil {
-		Error(w, err)
-	}
-	// Just need to write a response, http.StatusOK will be
-	// send automatically.
 }
 
 func main() {
 	vw := &vonage.VoiceWebhook{
-		Answer: http.HandlerFunc(Answer),
-		Event:  http.HandlerFunc(Event),
+		Answer: ReturnHandlerFunc(Answer),
+		Event:  ReturnHandlerFunc(Event),
 	}
 	addr := ":8080"
 	srv := &http.Server{
